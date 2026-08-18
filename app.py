05336ba1bc5015a50fa3d92975488dbc8bd2134e
tarihcituranx <tarihcituranx@users.noreply.github.com>
2026-08-18 22:44:41 +0300

    feat: return original_filename in /upload response to identify TV model

--- app.py
+++ app.py
@@ -312,7 +312,12 @@ def upload():
         else:
             channels = scm_core.get_channels(filepath)
 
-        return jsonify({'session_id': session_id, 'channels': channels, 'brand': brand})
+        return jsonify({
+            'session_id': session_id, 
+            'channels': channels, 
+            'brand': brand,
+            'original_filename': file.filename
+        })
     except Exception as e:
         shutil.rmtree(tmpdir, ignore_errors=True)
         del _sessions[session_id]
